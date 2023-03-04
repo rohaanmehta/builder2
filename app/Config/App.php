dddf5bf8b4b6c9236d3e79ae785756adbbb6d751
rohaanmehta <47930181+rohaanmehta@users.noreply.github.com>
2023-03-04 16:29:13 +0530

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/builder';
+    public string $baseURL = 'https://rohaanmehta.github.io/builder2';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
